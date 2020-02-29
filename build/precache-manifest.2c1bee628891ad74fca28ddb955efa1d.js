6b803f7fc677adfaee9429391b95e08c7b9a9a50
Andrew Heine <aheine@atrium.co>
2020-02-29 14:30:31 -0800

    first commit, create-react-app

---
+++ build/precache-manifest.2c1bee628891ad74fca28ddb955efa1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0467489796c65adc3bbe5220b0c0a27e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d4bfd3612818b0db45b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d1ad7f722e63fe18dddd",
+    "url": "/static/js/2.bc6928c0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.bc6928c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d4bfd3612818b0db45b",
+    "url": "/static/js/main.b5cb8d63.chunk.js"
+  },
+  {
+    "revision": "f483d1f7cd20d8e3322f",
+    "url": "/static/js/runtime-main.70302f69.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
